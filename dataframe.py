b6651886e6bdba9465882caf44cbfcfeddf08f51
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2023-06-17 19:11:13 +0900

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -300,7 +300,7 @@ def dataframe(level, player_id):
     cursor.execute(sql.format(level, player_id))
     raw = cursor.fetchall()
 
-    df=pd.DataFrame([[item.decode('utf-8') if isinstance(item, bytes) else item for item in row] for row in raw],
+    df=pd.DataFrame([[item.decode('latin-1') if isinstance(item, bytes) else item for item in row] for row in raw],
                     columns = ['game_id','pitch_type', 'game_date', 'release_speed', 'release_pos_x', 'release_pos_z',  'player_name', 'batname', 'batter', 'pitcher', 'events', 'description', 'zone', 'des', 'stand', 'p_throw', 'pitcherteam','batterteam', 'home_team' , 'away_team',
                                     'type', 'bb_type', 'balls', 'strikes', 'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'out_when_up', 'inning', 'inning_topbot', 'hit_distance_sc',
                                     'launch_speed','launch_angle','release_spin_rate','release_extension',
