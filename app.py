6b83bba825443c30650aabe140e050256125e35d
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2023-07-05 14:10:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,23 +84,6 @@ def show_main_page():
         sidebar_text = '<p style="font-family:sans-serif; color:gray; font-size: 14px;">(팀 / 선수 / 리그 선택시 자동실행)</p>'
         st.sidebar.markdown(sidebar_text, unsafe_allow_html=True)
 
-        # @st.cache_data
-        # def square(x):
-        #     return x**2
-
-        # @st.cache_data
-        # def cube(x):
-        #     return x**3
-
-        # if st.sidebar.button("RESET PAGE"):
-        #     # Clear values from *all* memoized functions:
-        #     # i.e. clear values from both square and cube
-        #     st.cache_data.clear()
-        #     show_login_page()
-
-        # if st.sidebar.button("RESET PAGE"):
-        #     st.experimental_rerun
-        
         if option != "-":
             league = select_league(option)
 
@@ -259,10 +242,6 @@ def show_main_page():
 
             spraychart_fig = season_spraychart(spraychart_dataframe)
             st.divider()
-
-
-
-
 
 with headerSection:
     if 'loggedIn' not in st.session_state:
