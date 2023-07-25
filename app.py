630d5d27febf6c2398b43645f33eff0cd499f0a1
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2023-07-25 14:53:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def show_login_page():
         if not st.session_state['loggedIn']:
             userName = st.text_input(label="", value="", placeholder="ID를 입력하시오.")
             password = st.text_input(label="", value="", placeholder="비밀번호를 입력하시오.", type="password")
-            st.form_submit_button("로그인", on_click=LoggedIn_Clicked, args=(userName, password))
+            
+            if st.form_submit_button("로그인"):
+                LoggedIn_Clicked(userName, password))
 
             # if submitted:
             #     LoggedIn_Clicked(userName, password)
