f3212b0c44598b08d57efbf623a5509cd3156c1b
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2023-07-26 16:49:36 +0900

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+# from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 # import os
 # from git import Repo
