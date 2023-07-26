5a66cc344804e830223425cf337097c1609c5ed9
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2023-07-26 13:51:10 +0900

    Update user.py

--- user.py
+++ user.py
@@ -39,8 +39,3 @@ def update_log(userName):
     # Save the log to a CSV file
     updated_log.to_csv('./login_log.csv', index=False, encoding='utf-8')
 
-    cd local_git_folder
-    git add login_log.csv
-    git commit -m 'my commit message'
-    git push 
-    
