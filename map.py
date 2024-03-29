ada6d06d5630644947c7ff4e228420cccc3874b0
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2024-03-29 14:21:05 +0900

    Update map.py

--- map.py
+++ map.py
@@ -543,24 +543,11 @@ def season_hangtime_spraychart(dataframe):
     col_index = len(dataframe['game_year'].unique())
 
     season_spraychart_fig = px.scatter(dataframe, x='groundX', y='groundY', color='hangtime_type', color_discrete_map=colors, facet_col='game_year', symbol="pitch_name",
-                         hover_name="player_name", hover_data=["rel_speed(km)","pitch_name","events","exit_velocity","description","launch_speed_angle","launch_angle",'hit_spin_rate'],
+                         hover_name="player_name", hover_data=["rel_speed(km)","pitch_name","events","exit_velocity","description","launch_speed_angle","launch_angle",'hit_spin_rate','hangtime'],
                         #  category_orders={"game_year": [2021,2022, 2023]},
                          height = 580, width = col_index*500)
     
     for i, d in enumerate(season_spraychart_fig.data):
-        # if hasattr(d, 'name') and isinstance(d.name, str) and ', ' in d.name:
-        #     split_name = d.name.split(', ')
-        #     symbol_name = split_name[1]
-        # # Check if the symbol name exists in the symbols dictionary
-        #     if symbol_name in symbols:
-        #         season_spraychart_fig.data[i].marker.symbol = symbols[symbol_name]
-        #     else:
-        #     # Handle case where symbol name doesn't exist in the symbols dictionary
-        #         print(f"Symbol '{symbol_name}' not found in symbols dictionary.")
-        # else:
-        # # Handle case where 'name' attribute doesn't exist or has incorrect format
-        #     print(f"Invalid name attribute for data point {i}: {d.name}")
-       
         season_spraychart_fig.data[i].marker.symbol = symbols[season_spraychart_fig.data[i].name.split(', ')[1]]
 
     season_spraychart_fig.update_yaxes(domain=[0.1, 0.97])
